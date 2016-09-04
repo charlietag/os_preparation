e2849e4535454b4cf523a89a0c9abd0a851d8b2e
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-04 23:54:09 +0800

    add comment print

--- functions/F_00_echo_path.sh
+++ functions/F_00_echo_path.sh
@@ -1,8 +1,10 @@
 #-----------Lib Use----------
+echo "-----------lib use only--------"
 echo "CURRENT_FOLDER : ${CURRENT_FOLDER}"
 echo "FUNCTIONS      : ${FUNCTIONS}"
 
 #-----------Function Use----------
+echo "-----------function use only--------"
 echo "TEMPLATES      : ${TEMPLATES}"
 echo "TMP            : ${TMP}"
 echo "CONFIG_FOLDER  : ${CONFIG_FOLDER}"
