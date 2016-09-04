ddb8c2216c598b5bb8e11b1c51b75791e11b6ca8
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-04 23:49:13 +0800

    change filename convention

--- lib/app.sh
+++ lib/app.sh
@@ -6,8 +6,10 @@ LIB="$(dirname $(readlink -m $0))/lib"
 #------------------------------------
 # Include libaries
 #------------------------------------
-LIB_SCRIPTS="$(ls $LIB |grep -E "^L[0-9][0-9]_[^[:space:]]+(.sh)$")"
+LIB_SCRIPTS="$(ls $LIB |grep -E "^L_[0-9][0-9]_[^[:space:]]+(.sh)$" | sort -n)"
 for LIB_SCRIPT in $LIB_SCRIPTS
 do
   . $LIB/$LIB_SCRIPT
+  #echo "$LIB_SCRIPT"
 done
+#exit
