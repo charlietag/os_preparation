15f512fb3f0dc4129e417a2dec88d341afa700a6
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-04 21:15:58 +0800

    change for executing functinos automatically

--- start.sh
+++ start.sh
@@ -4,14 +4,3 @@
 #*****************************************************************************
 . "$(dirname $(readlink -m $0))/lib/app.sh"
 
-print_path
-
-if [ -z "$PREDEFINED" ]
-then
-  echo "Predefined"
-#environment
-#install_mariadb
-#install_php7
-#install_ruby
-#install_nginx
-fi
