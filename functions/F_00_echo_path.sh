764e41683afbfde54a8f5886f2665dca268ebc92
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-04 23:46:38 +0800

    change structure

---
+++ functions/F_00_echo_path.sh
@@ -0,0 +1,8 @@
+#-----------Lib Use----------
+echo "CURRENT_FOLDER : ${CURRENT_FOLDER}"
+echo "FUNCTIONS      : ${FUNCTIONS}"
+
+#-----------Function Use----------
+echo "TEMPLATES      : ${TEMPLATES}"
+echo "TMP            : ${TMP}"
+echo "CONFIG_FOLDER  : ${CONFIG_FOLDER}"
--- functions/print_path.sh
+++
@@ -1,7 +0,0 @@
-echo $CURRENT_FOLDER
-echo $TEMPLATES     
-echo $CONFIGS       
-echo $FUNCTIONS     
-
-echo $CONFIG_FOLDER
-ls -a $CONFIG_FOLDER
