eb2466009ea1e8a2c1661b7f5b8a192216935799
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-07 06:04:54 +0800

    fix content

--- functions/F_00_echo_path.sh
+++ functions/F_00_echo_path.sh
@@ -4,18 +4,17 @@ echo "CURRENT_SCRIPT : ${CURRENT_SCRIPT}"
 echo "CURRENT_FOLDER : ${CURRENT_FOLDER}"
 echo "FUNCTIONS      : ${FUNCTIONS}"
 echo "LIB            : ${LIB}"
-echo "LIB_SCRIPTS    : ${LIB_SCRIPTS[@]}"
-echo "MAKE_FUNC      : ${MAKE_FUNC}"
+echo ""
 
 #-----------Function Use----------
 echo "-----------function use only--------"
 echo "TEMPLATES      : ${TEMPLATES}"
 echo "TMP            : ${TMP}"
 echo "CONFIG_FOLDER  : ${CONFIG_FOLDER}"
-echo "DATABAG_FILE   : ${DATABAG_FILE}"
+echo ""
 
 #-----------Function Use -predefined vars----------
 echo "-----------function use only - predefined vars--------"
-echo "FUNC_NAME      : ${FUNC_NAME}"
 echo "FIRST_ARGV     : ${FIRST_ARGV}"
 echo "ALL_ARGVS      : ${ALL_ARGVS[@]}"
+echo ""
