d91fec0adec9e87b5f86627794efbbec680f8a4a
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-08 00:37:30 +0800

    fix folder issure

--- functions/F_05_setup_nginx.sh
+++ functions/F_05_setup_nginx.sh
@@ -5,18 +5,20 @@ echo "========================================="
 echo "      setup nginx configs"
 echo "========================================="
 local nginx_confs=($(find ${CONFIG_FOLDER} -type f))
-local nginx_path=""
+local nginx_target=""
+local nginx_target_folder=""
 for nginx_conf in ${nginx_confs[@]}
 do
-  nginx_path="$(head -n 1 $nginx_conf | sed 's/^#//')"
+  nginx_target="$(head -n 1 $nginx_conf | sed 's/^#//')"
+  nginx_target_folder="$(dirname $nginx_target)"
   #echo "$nginx_conf"
-  #echo "$nginx_path"
-  test -d $nginx_path || mkdir -p $nginx_path
+  #echo "$nginx_target"
+  test -d $nginx_target_folder || mkdir -p $nginx_target_folder
   if [ "$(basename $nginx_conf)" == "passenger.conf" ]
   then
-    RENDER_CP $nginx_conf $nginx_path
+    RENDER_CP $nginx_conf $nginx_target
   else
-    \cp -a --backup=t $nginx_conf $nginx_path
+    \cp -a --backup=t $nginx_conf $nginx_target
   fi
 done
 
