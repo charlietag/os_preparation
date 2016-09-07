def7f08d196bb79bac7efa438b636178accf3fe1
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-07 21:50:55 +0800

    add cp config files

--- functions/F_04_install_nginx.sh
+++ functions/F_04_install_nginx.sh
@@ -1,12 +1,25 @@
 #--------------------------
 #  Prepare My Nginx Env
 #--------------------------
-mkdir /opt_home
-useradd -d /opt_home/optnginx -s /sbin/nologin -m -r optnginx
-useradd -d /opt_home/optpass -s /sbin/nologin -m -r optpass
+#mkdir /opt_home
+#useradd -d /opt_home/optnginx -s /sbin/nologin -m -r optnginx
+#useradd -d /opt_home/optpass -s /sbin/nologin -m -r optpass
+#
+##--------------------------
+##  Install via ruby gem
+##--------------------------
+#gem install passenger
+#passenger-install-nginx-module --auto-download --languages ruby --auto
 
 #--------------------------
-#  Install via ruby gem
+#  Setup nginx sample configs
 #--------------------------
-gem install passenger
-passenger-install-nginx-module --auto-download --languages ruby --auto
+local nginx_confs=($(find ${CONFIG_FOLDER} -type f))
+local nginx_path=""
+for nginx_conf in ${nginx_confs[@]}
+do
+  nginx_path="$(head -n 1 $nginx_conf | sed 's/^#//')"
+  echo "$nginx_conf"
+  echo "$nginx_path"
+  #cp -a $nginx_conf $nginx_path
+done
