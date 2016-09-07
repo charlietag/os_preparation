ccb21ea24ddbb12d808d4104a0537ac37cb18dcd
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-07 23:32:32 +0800

    add more comments

--- functions/F_04_install_nginx.sh
+++ functions/F_04_install_nginx.sh
@@ -1,19 +1,35 @@
 #--------------------------
 #  Prepare My Nginx Env
 #--------------------------
+
+echo "========================================="
+echo "      mkdir /opt_home"
+echo "========================================="
 mkdir /opt_home
+echo "========================================="
+echo "      add user \"optnginx\", \"optpass\" "
+echo "========================================="
 useradd -d /opt_home/optnginx -s /sbin/nologin -m -r optnginx
 useradd -d /opt_home/optpass -s /sbin/nologin -m -r optpass
 
 #--------------------------
 #  Install via ruby gem
 #--------------------------
+echo "========================================="
+echo "      gem install passenger"
+echo "========================================="
 gem install passenger
+echo "========================================="
+echo "     passenger-install-nginx-module --auto-download --languages ruby --auto "
+echo "========================================="
 passenger-install-nginx-module --auto-download --languages ruby --auto
 
 #--------------------------
 #  Setup nginx sample configs
 #--------------------------
+echo "========================================="
+echo "      setup nginx configs"
+echo "========================================="
 local nginx_confs=($(find ${CONFIG_FOLDER} -type f))
 local nginx_path=""
 for nginx_conf in ${nginx_confs[@]}
