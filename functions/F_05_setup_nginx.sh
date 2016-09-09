22a40cf08a3a1678f76ee9e3e1c48a93e0154778
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-09 08:43:15 +0800

    fetch realpath using string substitution

--- functions/F_05_setup_nginx.sh
+++ functions/F_05_setup_nginx.sh
@@ -9,10 +9,15 @@ local nginx_target=""
 local nginx_target_folder=""
 for nginx_conf in ${nginx_confs[@]}
 do
-  nginx_target="$(head -n 1 $nginx_conf | sed 's/^#//')"
+  nginx_target="${nginx_conf/${CONFIG_FOLDER}/}"
   nginx_target_folder="$(dirname $nginx_target)"
+  #nginx_target_head="$(head -n 1 $nginx_conf | sed 's/^#//')"
+
   #echo "$nginx_conf"
   #echo "$nginx_target"
+  #echo "$nginx_target_folder"
+  #echo "$nginx_target_head"
+
   test -d $nginx_target_folder || mkdir -p $nginx_target_folder
   if [ "$(basename $nginx_conf)" == "passenger.conf" ]
   then
