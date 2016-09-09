0b96eb891237e646ab8c6616c655e0d2347b6888
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-10 01:04:58 +0800

    default turn service off after installation, except control with toggle

--- functions/F_07_create_rails.sh
+++ functions/F_07_create_rails.sh
@@ -5,4 +5,7 @@ cd /home
 rails new myrails -d mysql #Create rails project, to verify
 cd /home/myrails
 chown -R optpass.optpass log tmp
+
+systemctl start mariadb.service
 bundle exec rails db:create:all
+systemctl stop mariadb.service
