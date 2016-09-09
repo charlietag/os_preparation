ad0dec3f56a852263f18231f47c09eaa41fbe894
Charlie Hsieh <charlieofficial@gmail.com>
2016-09-09 08:07:37 +0800

    remove cd back to current folder

--- functions/F_00_environment.sh
+++ functions/F_00_environment.sh
@@ -107,7 +107,6 @@ git clone https://github.com/vim-airline/vim-airline.git
 #Show git branch name
 git clone git://github.com/tpope/vim-fugitive.git
 #git clone git://github.com/airblade/vim-gitgutter.git
-cd $CURRENT_FOLDER
 
 #-----------------------------------------------------------------------------------------
 #Finish and Reboot
