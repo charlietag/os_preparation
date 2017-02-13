d931b64ed63f14d3ad8dc1ac25e7b0a90109bb07
Charlie Hsieh <charlieofficial@gmail.com>
2017-02-13 08:28:27 +0800

    solve code repeat

--- functions/F_01_ENV_02_repo.sh
+++ functions/F_01_ENV_02_repo.sh
@@ -2,10 +2,32 @@
 # YUM Repo
 #-----------------------------------------------------------------------------------------
 local centos_repo="/etc/yum.repos.d/"
-local repos=($(ls $centos_repo |grep -E "webtatic|MariaDB|node"))
+local repo_fail=0
+local repos=(webtatic MariaDB node)
 
+# =============================
+# check repo exists function
+# =============================
+check_repo_exist (){
+  for repo in ${repos[@]}
+  do
+    if [ -z "$(ls $centos_repo |grep -E "${repo}")" ]
+    then
+      echo "failed to find repo \"${repo}\""
+      repo_fail=1
+    fi
+  done
+}
+
+# =============================
+#Make sure repo exists before running
+# =============================
+check_repo_exist
+
+# =============================
 # Install repo if not exists
-if [ -z "${repos}" ]
+# =============================
+if [ $repo_fail -eq 1 ]
 then
   #PHP 7
   rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
@@ -18,14 +40,18 @@ then
   cp -a $CONFIG_FOLDER/yum_repo/*.repo $centos_repo
 fi
 
+# =============================
 #Make sure repo exists before running
-repos=($(ls $centos_repo |grep -E "webtatic|MariaDB|node"))
-if [ -z "${repos}" ]
+# =============================
+check_repo_exist
+if [ $repo_fail -eq 1 ]
 then
-  echo "Some repos not exists!"
+  echo "Some repo is not installed properly!"
   exit
 fi
 
+# =============================
 # Clean yum repo
+# =============================
 yum clean all
 
