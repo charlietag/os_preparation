d9892adb30447b182c17f3b14114ae2a50edd7cc
Charlie Hsieh <charlieofficial@gmail.com>
2017-02-15 14:29:27 +0800

    add other usage for rails db:create:all

--- functions/F_09_create_rails.sh
+++ functions/F_09_create_rails.sh
@@ -7,5 +7,9 @@ cd /home/myrails
 chown -R optpass.optpass log tmp
 
 systemctl start mariadb
+
+# Create PROD(require database.yml info. so PROD will failed due to wrong username/password for mysql db server), DEV, TEST env database in mysql
+# bundle exec rails db:create:all
+
 bundle exec rails db:create
 systemctl stop mariadb
