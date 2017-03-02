0448ce9308290e85498394230ce36592d264ea0e
Charlie Hsieh <charlieofficial@gmail.com>
2017-03-02 22:05:36 +0800

    add nginx limit_req_zone, and move side config file into folder include.d

--- functions/F_06_00_setup_nginx.sh
+++ functions/F_06_00_setup_nginx.sh
@@ -11,19 +11,8 @@ for nginx_conf in ${nginx_confs[@]}
 do
   nginx_target="${nginx_conf/${CONFIG_FOLDER}/}"
   nginx_target_folder="$(dirname $nginx_target)"
-  #nginx_target_head="$(head -n 1 $nginx_conf | sed 's/^#//')"
-
-  #echo "$nginx_conf"
-  #echo "$nginx_target"
-  #echo "$nginx_target_folder"
-  #echo "$nginx_target_head"
 
   test -d $nginx_target_folder || mkdir -p $nginx_target_folder
-  if [ "$(basename $nginx_conf)" == "passenger.conf" ]
-  then
-    RENDER_CP $nginx_conf $nginx_target
-  else
-    \cp -a --backup=t $nginx_conf $nginx_target
-  fi
+  \cp -a --backup=t $nginx_conf $nginx_target
 done
 
---
+++ functions/F_06_01_setup_nginx_include.sh
@@ -0,0 +1,18 @@
+#--------------------------
+#  Setup nginx sample configs
+#--------------------------
+echo "========================================="
+echo "      setup nginx configs"
+echo "========================================="
+local nginx_confs=($(find ${CONFIG_FOLDER} -type f))
+local nginx_target=""
+local nginx_target_folder=""
+for nginx_conf in ${nginx_confs[@]}
+do
+  nginx_target="${nginx_conf/${CONFIG_FOLDER}/}"
+  nginx_target_folder="$(dirname $nginx_target)"
+
+  test -d $nginx_target_folder || mkdir -p $nginx_target_folder
+  RENDER_CP $nginx_conf $nginx_target
+done
+
