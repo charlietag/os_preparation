03fe502acbbbff378601b7edac1cc21dd242cba0
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-12 13:08:55 +0800

    add helper for copy copy usage

---
+++ helpers/helper_copy_using_cp.sh
@@ -0,0 +1,19 @@
+echo "-----------"
+echo "copy config files"
+echo "copy all files under ${CONFIG_FOLDERi}"
+echo "including folders"
+echo "-----------"
+local current_confs=($(find ${CONFIG_FOLDER} -type f))
+local current_target=""
+local current_target_folder=""
+for current_conf in ${current_confs[@]}
+do
+  current_target="${current_conf/${CONFIG_FOLDER}/}"
+  current_target_folder="$(dirname $current_target)"
+
+  test -d $current_target_folder || mkdir -p $current_target_folder
+
+  echo "Setting up config file \"${current_target}\"......"
+  \cp -a --backup=t $current_conf $current_target
+done
+
