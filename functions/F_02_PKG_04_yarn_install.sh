068fe77b903f4debb9287acffffe79e3daad8a66
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-12 13:12:19 +0800

    classying folder structure

---
+++ functions/F_02_PKG_04_yarn_install.sh
@@ -0,0 +1,2 @@
+#For Rails 5.1+ , which is supporting yarn
+yum install -y yarn
--- functions/F_06_02_setup_nginx_vim.sh
+++ functions/F_06_02_setup_nginx_vim.sh
@@ -11,20 +11,5 @@
 
 #--------------------------
 # The following setup the same for optnginx under $HOME/.vim/
-echo "-----------"
-echo "setup nginx vim syntax"
-echo "-----------"
-local nginx_confs=($(find ${CONFIG_FOLDER} -type f))
-local nginx_target=""
-local nginx_target_folder=""
-for nginx_conf in ${nginx_confs[@]}
-do
-  nginx_target="${nginx_conf/${CONFIG_FOLDER}/}"
-  nginx_target_folder="$(dirname $nginx_target)"
-
-  test -d $nginx_target_folder || mkdir -p $nginx_target_folder
-
-  echo "Setting up config file \"${nginx_target}\"......"
-  cat $nginx_conf > $nginx_target
-done
-
+#--------------------------
+helper_cp_cat
