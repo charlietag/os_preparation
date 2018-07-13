a992c03f5d0c9b06865d957cef7574b859e7f1e8
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-14 02:19:39 +0800

    add tmpfile.d file for php/puma sock

---
+++ functions/F_02_PKG_05_php_05_tempfile_dir_for_sock.sh
@@ -0,0 +1,9 @@
+# =====================
+# Enable databag
+# =====================
+# RENDER_CP
+
+#--------------------------
+#  Setup tmpfiles.d configs
+#--------------------------
+helper_copy_using_render
