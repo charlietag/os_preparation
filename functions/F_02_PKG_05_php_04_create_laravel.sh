1d4852b3b3b7766fd4f51b0733eb5469e17f90ae
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-13 21:38:34 +0800

    fix composer permission issue

--- functions/F_02_PKG_05_php_04_create_laravel.sh
+++ functions/F_02_PKG_05_php_04_create_laravel.sh
@@ -7,8 +7,6 @@ echo "========================================="
 echo "(Laravel:${laravel_version}) composer create-project ---> mylaravel"
 echo "========================================="
 
-test -d $laravel_sites || mkdir -p $laravel_sites
+su -l $current_user -c "test -d ${laravel_sites} || mkdir -p ${laravel_sites}"
 
-cd $laravel_sites
-
-su -l $current_user -c "composer create-project --prefer-dist laravel/laravel mylaravel \"${laravel_version}\""
+su -l $current_user -c "cd $laravel_sites && composer create-project --prefer-dist laravel/laravel mylaravel \"${laravel_version}\""
