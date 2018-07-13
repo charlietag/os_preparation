c8b5ea8dbb28ce9147efeff3c7d32e7c9c949600
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-13 16:10:07 +0800

    done with yarn

--- functions/F_02_PKG_04_yarn_install.sh
+++ functions/F_02_PKG_04_yarn_install.sh
@@ -1,2 +1,10 @@
+# =====================
+# Enable databag
+# =====================
+# RENDER_CP
+
+#Yarn
+curl "${yarn_yum_repo}" -o /etc/yum.repos.d/yarn.repo
+
 #For Rails 5.1+ , which is supporting yarn
 yum install -y yarn
