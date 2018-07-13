21520cff3e80f254a54352ebe9cc62388e4ba98b
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-13 12:36:05 +0800

    finished nginx installation

--- functions/F_02_PKG_02_nginx_01_install.sh
+++ functions/F_02_PKG_02_nginx_01_install.sh
@@ -1,26 +1,11 @@
-#--------------------------
-#  Prepare My Nginx Env
-#--------------------------
+# =====================
+# Enable databag
+# =====================
+# RENDER_CP
 
-echo "========================================="
-echo "      mkdir /opt_home"
-echo "========================================="
-mkdir /opt_home
-echo "========================================="
-echo "      add user \"optnginx\", \"optpass\" "
-echo "========================================="
-useradd -d /opt_home/optnginx -s /sbin/nologin -m -r optnginx
-useradd -d /opt_home/optpass -s /sbin/nologin -m -r optpass
+echo "==============================="
+echo "        Render nginx repo"
+echo "==============================="
+helper_copy_using_render
 
-#--------------------------
-#  Install nginx via ruby gem using passenger-install
-#--------------------------
-echo "========================================="
-echo "      gem install passenger"
-echo "========================================="
-gem install passenger
-echo "========================================="
-echo "     passenger-install-nginx-module --auto-download --languages ruby --auto "
-echo "========================================="
-passenger-install-nginx-module --auto-download --languages ruby --auto
-
+yum install -y nginx
