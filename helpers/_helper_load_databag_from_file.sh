cdc48bc9b6942fc3c16acfa632666887fa4678fe
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-15 13:06:53 +0800

    change err message

--- helpers/_helper_load_databag_from_file.sh
+++ helpers/_helper_load_databag_from_file.sh
@@ -1,7 +1,7 @@
 local src_file="${DATABAG}/$1"
 
 if [[ ! -s "${src_file}" ]]; then
-  echo "FAILED: Databag file \"${src_file}\" not exists!"
+  echo "FAILED: Databag file \"${src_file}\" not exists or is empty !"
   exit
 fi
 . ${src_file}
