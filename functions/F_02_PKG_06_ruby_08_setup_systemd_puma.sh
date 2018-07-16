53b28c538efc69279fc5c9fc7966b9887e7ae383
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-16 12:17:02 +0800

    add puma - tool and systemd

---
+++ functions/F_02_PKG_06_ruby_08_setup_systemd_puma.sh
@@ -0,0 +1,12 @@
+# =====================
+# Enable databag
+# =====================
+# RENDER_CP
+
+#--------------------------
+#  Setup configs
+#--------------------------
+task_copy_using_cat
+
+sed -i "s/RAILS_SERVICE_USERNAME/${current_user}/g" /usr/lib/systemd/system/puma.service
+sed -i "s/RAILS_SERVICE_USERNAME/${current_user}/g" /usr/local/bin/puma-mgr
