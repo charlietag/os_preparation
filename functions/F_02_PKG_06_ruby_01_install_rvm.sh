0160d943b092a21f33cf1dd687ee4fd6d719fc7e
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-17 08:07:53 +0800

    add retry to rvm installation, due to RVM link is always timeout

--- functions/F_02_PKG_06_ruby_01_install_rvm.sh
+++ functions/F_02_PKG_06_ruby_01_install_rvm.sh
@@ -6,22 +6,44 @@
 echo "========================================="
 echo "      Install RVM"
 echo "========================================="
-su -l $current_user -c "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
-if [[ $? -ne 0 ]]; then
-  echo "rvm gpg keyserver installation failed!"
-  exit
-fi
+local rvm_install_retry=5
 
-su -l $current_user -c "\curl -sSL https://get.rvm.io | bash -s ${rvm_version}"
-echo ""
 
-local rvm_check="$(su -l $current_user -c "which rvm 2>/dev/null")"
-if [[ -z "${rvm_check}" ]]; then
-  echo "rvm installation failed!"
-  exit
-fi
+############### Install RVM retry Loop #############
+let rvm_install_retry++
+for ((i=1; i<=rvm_install_retry; i++)); do
 
-# rvm ctrl-c trap soltion workaround
+  # ---------- Check RVM Installation -----------
+  local rvm_check="$(su -l $current_user -c "which rvm 2>/dev/null")"
+  if [[ -n "${rvm_check}" ]]; then
+    echo "RVM is installed successfully!"
+    break
+  fi
+
+  if [[ -z "${rvm_check}" ]]; then
+    echo "RVM is not installed yet!"
+    [[ $i -eq $rvm_install_retry ]] && exit
+  fi
+
+  echo -n "RVM installation (try: $i) "
+  sleep 1; echo -n "."; sleep 1; echo -n "."; sleep 1; echo -n "."; echo ""
+
+  # ---------- Remove gpg key and install gpg -----------
+  su -l $current_user -c "rm -fr ${current_user_home}/.gnupg"
+  su -l $current_user -c "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
+  if [[ $? -ne 0 ]]; then
+    echo "rvm gpg keyserver installation failed!"
+    continue
+  fi
+
+  # ---------- Install RVM -----------
+  su -l $current_user -c "\curl -sSL https://get.rvm.io | bash -s ${rvm_version}"
+done
+############### Install RVM retry Loop #############
+
+
+
+# ----- rvm ctrl-c trap soltion workaround ------
 echo "------------------------------------------"
 echo "RVM trap workaround"
 echo "modify rvm source code"
