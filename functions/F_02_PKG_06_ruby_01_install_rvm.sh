a88a5dbc88e16efae96ecd782bc51602672f8fa5
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-17 08:59:01 +0800

    delete folder using root

--- functions/F_02_PKG_06_ruby_01_install_rvm.sh
+++ functions/F_02_PKG_06_ruby_01_install_rvm.sh
@@ -29,7 +29,7 @@ for ((i=1; i<=rvm_install_retry; i++)); do
   sleep 1; echo -n "."; sleep 1; echo -n "."; sleep 1; echo -n "."; echo ""
 
   # ---------- Remove gpg key and install gpg -----------
-  su -l $current_user -c "rm -fr ${current_user_home}/.gnupg"
+  rm -fr ${current_user_home}/.gnupg
   su -l $current_user -c "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
   if [[ $? -ne 0 ]]; then
     echo "rvm gpg keyserver installation failed!"
