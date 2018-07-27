53454b8e749737d6c1ed7be38ce7612fd40f4601
Charlie Hsieh <charlieofficial@gmail.com>
2018-07-27 14:06:50 +0800

    add version note for docker-compose

--- functions/F_02_PKG_07_docker_02_indstall_docker-compose.sh
+++ functions/F_02_PKG_07_docker_02_indstall_docker-compose.sh
@@ -11,3 +11,18 @@ curl -L "${docker_compose_url}" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 docker-compose --version
+
+# ----------------------------------
+# Install latest version of docker-comopose
+# ----------------------------------
+local docker_compose_ver="/var/lib/docker-compose_version.txt"
+local docker_compose_ver_old="/var/lib/$(date +"%Y%m%d-%H%M%S")_docker-compose_version.txt"
+echo "---------------"
+echo "docker-compose version noted at ${docker_compose_ver}"
+echo "---------------"
+
+if [[ -s $docker_compose_ver ]]; then
+  \cp -a $docker_compose_ver "${docker_compose_ver_old}" && docker-compose -v > $docker_compose_ver
+else
+  docker-compose -v > $docker_compose_ver
+fi
