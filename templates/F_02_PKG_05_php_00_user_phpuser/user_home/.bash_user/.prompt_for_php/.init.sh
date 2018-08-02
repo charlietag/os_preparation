e44e2638b451ee9000b303d7aa5b9ea53bb0af6c
Charlie Hsieh <charlieofficial@gmail.com>
2018-08-02 17:30:03 +0800

    add php_prompt for php / laravel version

---
+++ templates/F_02_PKG_05_php_00_user_phpuser/user_home/.bash_user/.prompt_for_php/.init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#------------------------------------------------------
+#               Bash Prompt Setting - Function
+#------------------------------------------------------
+PROMPT_SYM='#'
+
+ORIGIN_PS="${PS1%\\n# }"
+unset PS1
+
+#------------------------------------------------------
+#             Bash Prompt Setting - PHP
+#------------------------------------------------------
+function set_php {
+  local php_dark_yellow="\e[33m"
+  local php_dark_cyan="\e[36m"
+  local php_color_end="\033[00m"
+
+  local laravel_ver=""
+  local php_prompt="${php_dark_yellow}($(php -v|head -n 1 |awk '{print $2}'))${php_color_end}"
+  local prompt_for_php="${php_prompt}"
+
+  [[ -f "composer.lock" ]] && laravel_ver="$(cat composer.lock |grep -A 1 'laravel/framework' | grep version |grep -Eo "[[:digit:]]+.[[:digit:]]+.[[:digit:]]+")"
+
+  if [[ -n "${laravel_ver}" ]]; then
+    laravel_ver="${php_dark_cyan}(${laravel_ver})${php_color_end}"
+    prompt_for_php="${prompt_for_php} ${laravel_ver}"
+  fi
+  echo -e "${prompt_for_php}"
+}
+
+
+#------------------------------------------------------
+#             Bash Prompt Setting - Start
+#------------------------------------------------------
+append_here="\$(set_php)"
+PS1="${ORIGIN_PS} ${append_here}\n${PROMPT_SYM} "
