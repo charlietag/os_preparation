2542742be00cb920b66437c85fbb2c43f812f24a
Charlie Hsieh <charlieofficial@gmail.com>
2018-08-08 08:45:26 +0800

    1. install rvm using hkp(gpg) port 80(default 11371. 2. to avoid installing behind firewall, git clone via https instead (before: git port 9418)

--- helpers/helper_env_user_base.sh
+++ helpers/helper_env_user_base.sh
@@ -26,15 +26,15 @@ mkdir -p ${current_user_home}/.vim/autoload ${current_user_home}/.vim/bundle && 
 curl -LSso ${current_user_home}/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 cd ${current_user_home}/.vim/bundle
-git clone git://github.com/godlygeek/tabular.git
+git clone https://github.com/godlygeek/tabular.git
 git clone https://github.com/Raimondi/delimitMate.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/vim-airline/vim-airline.git
-#git clone https://github.com/vim-airline/vim-airline-themes
+#git clone https://github.com/vim-airline/vim-airline-themes.git
 
 #Show git branch name
-git clone git://github.com/tpope/vim-fugitive.git
-#git clone git://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/tpope/vim-fugitive.git
+#git clone https://github.com/airblade/vim-gitgutter.git
 
 #-----------------------------------------------------------------------------------------
 #Make sure user_home priv is correct
