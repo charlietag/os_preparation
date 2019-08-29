34c8a696b4e800db72c9a1050496373f637c5b50
Charlie Hsieh <charlieofficial@gmail.com>
2019-08-30 07:08:16 +0800

    refine comment

--- functions/F_02_PKG_06_ruby_09_redmine_create.sh
+++ functions/F_02_PKG_06_ruby_09_redmine_create.sh
@@ -137,12 +137,14 @@ sleep 1; echo -n "."; sleep 1; echo -n "."; sleep 1; echo -n "."; echo ""
 systemctl start mariadb
 
 # Avoid difference command creating db between rails 4 , and rails 5, create db via mysql command
-# And some redmine plugins (like EasyGantt) seemt to crash command "rails rails db:create"
-if [[ -z "${redmine_db_pass}" ]]; then
-  mysql -u root -e "CREATE DATABASE ${redmine_db_name} CHARACTER SET utf8;"
-else
-  mysql -u root -p${redmine_db_pass} -e "CREATE DATABASE ${redmine_db_name} CHARACTER SET utf8;"
-fi
+# Also some redmine plugins (like EasyGantt) seemt to crash command "rails rails db:create"
+#if [[ -z "${redmine_db_pass}" ]]; then
+#  mysql -u root -e "CREATE DATABASE ${redmine_db_name} CHARACTER SET utf8;"
+#else
+#  mysql -u root -p${redmine_db_pass} -e "CREATE DATABASE ${redmine_db_name} CHARACTER SET utf8;"
+#fi
+
+## using command "rails db:create" can also prevent stange plugin (like EasyGantt) ruin redmine.
 
 # ====== Start to install redmine gem =======
 echo "========================================="
@@ -154,7 +156,7 @@ echo ""
 su -l $current_user -c "cd ${redmine_web_root} && bundle _${redmine_bundler_version}_ install --without development test"
 su -l $current_user -c "cd ${redmine_web_root} && bundle _${redmine_bundler_version}_ exec rake generate_secret_token"
 # su -l $current_user -c "cd ${redmine_web_root} && bundle _${redmine_bundler_version}_ exec rake db:create RAILS_ENV=production"  #---> rails 4 , or below
-#su -l $current_user -c "cd ${redmine_web_root} && bundle _${redmine_bundler_version}_ exec rails db:create RAILS_ENV=production"   #---> rails 5 , or above
+su -l $current_user -c "cd ${redmine_web_root} && bundle _${redmine_bundler_version}_ exec rails db:create RAILS_ENV=production"   #---> rails 5 , or above
 su -l $current_user -c "cd ${redmine_web_root} && bundle _${redmine_bundler_version}_ exec rake db:migrate RAILS_ENV=production"
 if [[ -n "${redmine_default_lang}" ]]; then
   su -l $current_user -c "cd ${redmine_web_root} && bundle _${redmine_bundler_version}_ exec rake redmine:load_default_data RAILS_ENV=production REDMINE_LANG=${redmine_default_lang}"
