553d56835776e839a0636cf87a2750019f9243f3
Charlie Hsieh <charlieofficial@gmail.com>
2019-11-20 10:10:16 +0800

    change redmine time format of log

--- templates/F_02_PKG_06_ruby_09_redmine_create/user_home/web_sites/redmine/config/additional_environment.rb
+++ templates/F_02_PKG_06_ruby_09_redmine_create/user_home/web_sites/redmine/config/additional_environment.rb
@@ -6,5 +6,43 @@
 #
 #   config.log_level = :debug
 #   ...
-#
 
+# -------------------------------------------------------------------------------------------------------------------------
+# Add timestamp (in system default timezone)
+# Fail2ban requires timestamp to calulate the findtime, etc.
+# Default redmine log authenication error in UTC
+# /home/rubyuser/rails_sites/redmine/app/controllers/account_controller.rb
+#  def invalid_credentials
+#    logger.warn "Failed login for '#{params[:username]}' from #{request.remote_ip} at #{Time.now.utc}"
+#    flash.now[:error] = l(:notice_account_invalid_credentials)
+#  end
+# And if it's logged in UTC, and my timezone is +0800,
+# findtime 600, will never find the failed attempped log.
+# -------------------------------------------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------------------------------------------
+# Solution 1:
+#   #{Time.now.utc} -> #{Time.now}
+# Log Format:
+#   "Failed login for 'asdf' from 59.124.165.113 at 2019-09-02 14:06:03 +0800"
+# -------------------------------------------------------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------------------------------------------------------
+# Solution 2:
+#   Change default log formatter as the following 2 methods
+# Log Format:
+#   "W, [2019-09-02T14:07:37.426871 #18632]  WARN -- : Failed login for 'afdfdasdf' from 59.124.165.113 at 2019-09-02 06:07:37 UTC"
+# -------------------------------------------------------------------------------------------------------------------------
+
+# --- Method 1: Rails Default config for Production ---
+config.log_formatter = ::Logger::Formatter.new
+
+# OR
+# --- Method 2: Rails To Overwrite Rails default log formatter ---
+# Add the following in file /home/rubyuser/rails_sites/redmine/config/environment.rb
+#class Logger
+#  def format_message(severity, timestamp, progname, msg)
+#    "#{timestamp} (#{$$}) #{msg}\n"
+#  end
+#end
