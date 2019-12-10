4fb52e496aff3be96809d92dd86140c84842ffe0
Charlie Hsieh <charlieofficial@gmail.com>
2019-12-10 23:38:54 +0800

    add more description about config.action_dispatch.trusted_proxies of rails config

--- templates/F_02_PKG_06_ruby_09_redmine_create/user_home/web_sites/redmine/config/additional_environment.rb
+++ templates/F_02_PKG_06_ruby_09_redmine_create/user_home/web_sites/redmine/config/additional_environment.rb
@@ -9,9 +9,37 @@
 
 # -------------------------------------------------------------------------------------------------------------------------
 # Avoid request.remote_ip always being 127.0.0.1 in production.log
-#   default rails will record remote_ip as 127.0.0.1 , if client is from private lan (192.168.x.x)
+#   default rails will record remote_ip as 127.0.0.1 , if client is from private network
+#   which means remote_ip would be replaced with 127.0.0.1, if you are connecting from private network
+#   Ref. https://github.com/rails/rails/blob/master/actionpack/lib/action_dispatch/middleware/remote_ip.rb
+#       TRUSTED_PROXIES = [
+#         "127.0.0.1",      # localhost IPv4
+#         "::1",            # localhost IPv6
+#         "fc00::/7",       # private IPv6 range fc00::/7
+#         "10.0.0.0/8",     # private IPv4 range 10.x.x.x
+#         "172.16.0.0/12",  # private IPv4 range 172.16.0.0 .. 172.31.255.255
+#         "192.168.0.0/16", # private IPv4 range 192.168.x.x
+#       ].map { |proxy| IPAddr.new(proxy) }
+#
+# -------------------------------------------------------------------------------------------------------------------------
+# Solution 1:
+#   force TRUSTED_PROXIES only localhost , not private network.
+# The following setting means only trust '127.0.0.1' , not entire private network
+#   Ref. https://guides.rubyonrails.org/configuring.html#configuring-middleware
 # -------------------------------------------------------------------------------------------------------------------------
 # config.action_dispatch.trusted_proxies = [IPAddr.new('127.0.0.1')]
+
+# -------------------------------------------------------------------------------------------------------------------------
+# ------------ DO NOT TRY THIS ------------
+# Solution 2 (This is a fake solution): "DO NOT TRY THIS"
+#   Disable ip_spoofing_check, will not stop the behavior of replacing remote_ip with '127.0.0.1'
+#     This would just disable the network defense...
+# config.action_dispatch.ip_spoofing_check = false      # DO NOT TRY THIS
+# ------------ DO NOT TRY THIS ------------
+# -------------------------------------------------------------------------------------------------------------------------
+
+
+
 
 # -------------------------------------------------------------------------------------------------------------------------
 # Add timestamp (in system default timezone)
