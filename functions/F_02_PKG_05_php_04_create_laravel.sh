b95c910c1d3fc73c1bd234c87bceb01806699464
Charlie Hsieh <charlieofficial@gmail.com>
2020-04-04 23:52:41 +0800

    fix typo

--- functions/F_02_PKG_05_php_04_create_laravel.sh
+++ functions/F_02_PKG_05_php_04_create_laravel.sh
@@ -9,4 +9,4 @@ echo "========================================="
 
 su -l $current_user -c "test -d ${web_sites} || mkdir -p ${web_sites}"
 
-su -l $current_user -c "cd $web_sites && composer create-project --prefer-dist laravel/laravel mylaravel \"${laravel_version}\""
+su -l $current_user -c "cd $web_sites && composer create-project --prefer-dist laravel/laravel mylaravel \"${laravel_version}.*\""
