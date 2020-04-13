64311b7f9360af8938e1397ce1040a6602da5154
Charlie Hsieh <charlieofficial@gmail.com>
2020-04-13 14:28:06 +0800

    adjust indent

--- functions/F_01_ENV_01_disable_srv.sh
+++ functions/F_01_ENV_01_disable_srv.sh
@@ -8,7 +8,12 @@
 #-----------------------------------------------------------------------------------------
 for disabled_service in ${disabled_services[@]}
 do
+  echo ""
+  echo "-----------------------------"
   echo "Stopping service ${disabled_service}......"
+  echo "-----------------------------"
   systemctl stop ${disabled_service}
   systemctl disable ${disabled_service}
+  echo ""
+  echo ""
 done
