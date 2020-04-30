922c6d3311400e77856655db648351fe68ab23c5
Charlie Hsieh <charlieofficial@gmail.com>
2020-04-30 15:12:07 +0800

    change for CentOS 8

--- functions/F_02_PKG_02_nginx_01_install.sh
+++ functions/F_02_PKG_02_nginx_01_install.sh
@@ -1,11 +1,15 @@
 # =====================
 # Enable databag
 # =====================
-# DATABAG_CFG:enable
+# DATABAG_CFG:disable
 
 echo "==============================="
 echo "        Render nginx repo"
 echo "==============================="
-task_copy_using_render
+if [[ -z "$(dnf repolist nginx-stable 2>/dev/null)" ]] ; then
+  #task_copy_using_render
+  task_copy_using_cat
+  L_UPDATE_REPO 5000
+fi
 
 dnf install -y nginx
