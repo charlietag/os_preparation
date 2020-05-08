62208d64dc0d893cd7ff2c25c4335561b78367cd
Charlie Hsieh <charlieofficial@gmail.com>
2020-05-08 17:08:32 +0800

    add some comment about timer (systemd)

--- functions/F_80_MA_dnf_automatic.sh
+++ functions/F_80_MA_dnf_automatic.sh
@@ -3,4 +3,6 @@ rpm --quiet -q dnf-automatic || dnf -y install dnf-automatic
 
 sed -Ei "s/^email_from = [[:print:]]+/email_from = root@$(hostname)/g" /etc/dnf/automatic.conf 
 
+# About timer in systemd (chinese infomation)
+  # https://www.ruanyifeng.com/blog/2018/03/systemd-timer.html
 systemctl enable --now dnf-automatic.timer
