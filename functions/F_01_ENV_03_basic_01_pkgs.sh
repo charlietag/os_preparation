d6fd9c4c28d3a972a561293743dab718e91a9719
Charlie Hsieh <charlieofficial@gmail.com>
2020-05-12 14:51:04 +0800

    add GeoIP GeoIP-devel packages

--- functions/F_01_ENV_03_basic_01_pkgs.sh
+++ functions/F_01_ENV_03_basic_01_pkgs.sh
@@ -55,25 +55,24 @@ pkgs_list="${pkgs_list} chrony"
 #Package Install
 #-----------------------------------------------------------------------------------------
 # For NodeJS
-#dnf install -y gcc-c++ make
 pkgs_list="${pkgs_list} gcc-c++ make"
 
 # Basic dev packages
 dnf groupinstall -y "Development Tools"
-#dnf install -y whois bash redhat-lsb screen git tree vim sysstat mtr net-tools wget openssl-devel bind-utils
 pkgs_list="${pkgs_list} whois bash redhat-lsb screen git tree vim sysstat mtr net-tools wget openssl-devel bind-utils"
 
 # Basic debug tools - Enhanced tail / Enhanced grep
-#dnf install -y multitail ack
 pkgs_list="${pkgs_list} multitail ack"
 
 # For SSL
-#dnf install -y openssl openssl-libs openssl-devel libticonv-devel
 pkgs_list="${pkgs_list} openssl openssl-libs openssl-devel"
 
 # For sql server connection (freetds)
-#dnf install -y freetds freetds-devel 
 pkgs_list="${pkgs_list} freetds freetds-devel"
+
+# For GeoIP purpose
+pkgs_list="${pkgs_list} GeoIP GeoIP-devel"
+
 
 
 #-----------------------------------------------------------------------------------------
