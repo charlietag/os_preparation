c669a0e5bd71a0f3006070ddea31ed768d350219
Charlie Hsieh <charlieofficial@gmail.com>
2020-05-29 14:01:13 +0800

    change for disabling IPV6

--- functions/F_01_ENV_02_os.sh
+++ functions/F_01_ENV_02_os.sh
@@ -16,20 +16,46 @@
 #   |__ will avoid /etc/resolv.conf contains search localdomain everytime reboot
 hostnamectl set-hostname ${host_name}
 
+# render os config (NetworkManager, sysctl.d)
+task_copy_using_render_sed
+
 # set ethernet card PEERDNS to "no" , avoid DHCP modify /etc/resolv.conf
 local eth_cards="$(ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v "lo$")"
 for eth_card in $eth_cards
 do
+  # avoid /etc/resolv.conf being changed by NM
+  sed -re '/DNS[[:digit:]]+=/d' -i $eth_card
   sed -i /PEERDNS/d $eth_card
+
   echo "PEERDNS=\"no\"" >> $eth_card
+  echo "IPV6_PEERDNS=\"no\"" >> $eth_card
+
+  # Totally disable IPV6
+  if [[ $disable_ipv6 -eq 1 ]] ; then
+    sed -i /IPV6/d $eth_card
+    echo "
+    IPV6_DISABLED=yes
+    IPV6INIT=no
+    IPV6_DEFROUTE=no
+    IPV6_FAILURE_FATAL=no
+    IPV6_ADDR_GEN_MODE=stable-privacy
+    IPV6_AUTOCONF=no
+    IPV6_PEERDNS=no
+    IPV6_PEERROUTES=no
+    " >> $eth_card
+  fi
+
+
+  # Avoid config NetworkManager is not activated immediately
+  # NetworkManager is like puma app server, you launch software NetworkManager , does not mean you trigger ifcfg reloaded
+  ifdown $eth_card ; ifup $eth_card
 done
 
-# nameserver
-RENDER_CP $CONFIG_FOLDER/etc/resolv.conf /etc/resolv.conf
 
-# Disable IPv6
-RENDER_CP $CONFIG_FOLDER/etc/sysctl.conf /etc/sysctl.conf
-sysctl -p
+
+# Activate config
+sysctl -p /etc/sysctl.d/z_custom_sysctl.conf
+systemctl restart NetworkManager
 
 #-----------------------------------------------------------------------------------------
 #SELINUX OFF
