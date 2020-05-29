c242693979940eed1dee0f0611f9d924ac63460b
Charlie Hsieh <charlieofficial@gmail.com>
2020-05-29 14:34:45 +0800

    fix typo

--- functions/F_01_ENV_02_os.sh
+++ functions/F_01_ENV_02_os.sh
@@ -51,7 +51,7 @@ do
   # Avoid config NetworkManager is not activated immediately
   # NetworkManager is like puma app server, you launch software NetworkManager , does not mean you trigger ifcfg reloaded
   local if_device="$(grep DEVICE ${eth_card} | cut -d'=' -f2)"
-  ifdown $eth_card ; ifup $eth_card
+  ifdown $if_device ; ifup $if_device
 done
 
 
