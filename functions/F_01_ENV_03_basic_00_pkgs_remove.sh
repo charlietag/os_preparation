d11adf6ef34d33a05fbe3e746833c7ab9fc38032
Charlie Hsieh <charlieofficial@gmail.com>
2020-06-08 14:19:22 +0800

    move fix_known_issue to single function

--- functions/F_01_ENV_03_basic_00_pkgs_remove.sh
+++ functions/F_01_ENV_03_basic_00_pkgs_remove.sh
@@ -15,9 +15,3 @@ for remove_pkg_name in ${remove_pkg_names[@]}; do
   fi
 done
 
-
-# -------- Solve dnf known issue --------
-#  Problem: conflicting requests
-#  - nothing provides module(perl:5.26) needed by module perl-DBI:1.641:8010020191113222731:16b3ab4d-0.x86_64
-dnf module enable perl:5.26/common -y
-
