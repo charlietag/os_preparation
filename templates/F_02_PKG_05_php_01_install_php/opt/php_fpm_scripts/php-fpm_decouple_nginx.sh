14ade318fa206bf2f47b52f3f072e0fb9b30113f
Charlie Hsieh <charlieofficial@gmail.com>
2020-06-13 22:28:17 +0800

    fix logical error

--- templates/F_02_PKG_05_php_01_install_php/opt/php_fpm_scripts/php-fpm_decouple_nginx.sh
+++ templates/F_02_PKG_05_php_01_install_php/opt/php_fpm_scripts/php-fpm_decouple_nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-FILE_SHOULD_BE_CHANGED="$(cat /usr/lib/systemd/system/nginx.service.d/php-fpm.conf  | grep 'Wants=php-fpm.service')"
+FILE_SHOULD_BE_CHANGED="$(cat /usr/lib/systemd/system/nginx.service.d/php-fpm.conf | grep -v '^\s*#' | grep 'Wants=php-fpm.service')"
 if [[ -n "${FILE_SHOULD_BE_CHANGED}" ]]; then
   sed -e 's/^#*/#/' -i /usr/lib/systemd/system/nginx.service.d/php-fpm.conf
   systemctl daemon-reload
