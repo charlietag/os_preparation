b113545f0b04631e233bd8eec5a22545afdd7d71
Charlie Hsieh <charlieofficial@gmail.com>
2020-06-13 16:21:57 +0800

    systemctl daemon-reload if systemd file is changed

--- templates/F_02_PKG_05_php_01_install_php/opt/php_fpm_scripts/php-fpm_decouple_nginx.sh
+++ templates/F_02_PKG_05_php_01_install_php/opt/php_fpm_scripts/php-fpm_decouple_nginx.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sed -e 's/^#*/#/' -i /usr/lib/systemd/system/nginx.service.d/php-fpm.conf
+FILE_SHOULD_BE_CHANGED="$(cat /usr/lib/systemd/system/nginx.service.d/php-fpm.conf  | grep 'Wants=php-fpm.service')"
+if [[ -n "${FILE_SHOULD_BE_CHANGED}" ]]; then
+  sed -e 's/^#*/#/' -i /usr/lib/systemd/system/nginx.service.d/php-fpm.conf
+  systemctl daemon-reload
+fi
