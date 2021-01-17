16afe079c7f02cb82e23e275ff6faac6ee1063e4
Charlie Hsieh <charlieofficial@gmail.com>
2021-01-18 00:40:52 +0800

    change for Puma-5 , systemd using type "notify" - step - classifying folder structure

--- functions/F_02_PKG_06_ruby_08_setup_systemd_puma.sh
+++ functions/F_02_PKG_06_ruby_08_setup_systemd_puma.sh
@@ -3,10 +3,16 @@
 # =====================
 # DATABAG_CFG:enable
 
-#--------------------------
-#  Setup configs
-#--------------------------
-task_copy_using_cat
+# Setup config and command
+task_copy_using_render_sed
 
-sed -i "s/RAILS_SERVICE_USERNAME/${current_user}/g" /usr/local/bin/puma-mgr
-chmod 755 /usr/local/bin/puma-mgr
+# ------------------------------------
+# NO NEED TO MASK : service template file move to folder, /usr/local/bin/puma-mgr_template
+#
+# Disable deprecated puma.service (Puma 5 - No daemon option)
+#systemctl mask puma
+# ------------------------------------
+
+
+# Make puma-systemd-mgr executable
+chmod 755 /usr/local/bin/puma-systemd-mgr
