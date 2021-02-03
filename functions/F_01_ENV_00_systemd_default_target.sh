959cf203aaed2747c652cf69499a705bbb23f179
Charlie Hsieh <charlieofficial@gmail.com>
2021-02-03 15:39:11 +0800

    add info about systemd target

--- functions/F_01_ENV_00_systemd_default_target.sh
+++ functions/F_01_ENV_00_systemd_default_target.sh
@@ -1,5 +1,30 @@
-# List all available targets
+# --- List all available targets ---
 #systemctl list-units --type=target --all
+
+# --- List target dependencies ---
+# # systemctl list-dependencies multi-user.target  --reverse
+# multi-user.target
+# ● └─graphical.target
+#
+# systemctl list-dependencies graphical.target  --reverse
+# graphical.target
+#
+# # --- Create a systemd target ---
+# # Ref. https://unix.stackexchange.com/questions/301987/how-to-create-a-systemd-target
+# find /usr/lib/systemd/system |grep multi
+# /usr/lib/systemd/system/multi-user.target.wants
+# /usr/lib/systemd/system/multi-user.target.wants/dbus.service
+# /usr/lib/systemd/system/multi-user.target.wants/getty.target
+# /usr/lib/systemd/system/multi-user.target.wants/systemd-ask-password-wall.path
+# /usr/lib/systemd/system/multi-user.target.wants/systemd-logind.service
+# /usr/lib/systemd/system/multi-user.target.wants/systemd-update-utmp-runlevel.service
+# /usr/lib/systemd/system/multi-user.target.wants/systemd-user-sessions.service
+# /usr/lib/systemd/system/multi-user.target.wants/plymouth-quit-wait.service
+# /usr/lib/systemd/system/multi-user.target.wants/plymouth-quit.service
+# /usr/lib/systemd/system/multi-user.target
+# /usr/lib/systemd/system/multipathd.service
+# /usr/lib/systemd/system/multipathd.socket
+
 
 
 local default_target="multi-user.target"
