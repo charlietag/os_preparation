af4f1a5b3afb3066873c03f68ca1f6b905eee9f1
Charlie Hsieh <charlieofficial@gmail.com>
2021-02-06 03:20:31 +0800

    fix err

--- functions/F_91_MA_disable_service.sh
+++ functions/F_91_MA_disable_service.sh
@@ -6,16 +6,8 @@
 #-----------------------------------------------------------------------------------------
 # Stop and disable services
 #-----------------------------------------------------------------------------------------
-# Do "F_01_ENV_01_disable_srv.sh" again, to make sure to disable no use services
+echo 'Do "F_01_ENV_01_disable_srv.sh" again, to make sure to disable no use services'
+echo ""
 
-for disabled_service in ${disabled_services[@]}
-do
-  echo ""
-  echo "-----------------------------"
-  echo "Stopping service ${disabled_service}......"
-  echo "-----------------------------"
-  systemctl stop ${disabled_service}
-  systemctl disable ${disabled_service}
-  echo ""
-  echo ""
-done
+# call function: F_01_ENV_01_disable_srv
+F_01_ENV_01_disable_srv
