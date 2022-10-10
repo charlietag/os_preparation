aae52fa3918fb3e131e1c1c35b688383a9f723a9
Charlie Hsieh <charlieofficial@gmail.com>
2022-10-10 23:08:42 +0800

    add container tools

--- functions/F_01_ENV_03_basic_04_pkgs_container.sh
+++ functions/F_01_ENV_03_basic_04_pkgs_container.sh
@@ -3,3 +3,5 @@ echo "  Installing Container Management packages..."
 echo "==============================="
 
 dnf groupinstall -y "Container Management"
+
+dnf install -y podman buildah skopeo runc conmon criu udica container-selinux
